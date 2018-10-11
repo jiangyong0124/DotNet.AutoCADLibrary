b66f14471a51259272f887d133b459cac9db99c6
SmallAcmen <43090622+SmallAcmen@users.noreply.github.com>
2018-10-12 07:20:49 +0800

    1

---
+++ DotNet.AutoCADLibrary/DotNet_AutoCAD_API_Helper/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_AutoCAD_API_Helper
+{
+    public class Class1
+    {
+
+    }
+}
